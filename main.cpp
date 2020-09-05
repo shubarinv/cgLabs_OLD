cf3cc1067f2fa6e0bba6658ae98432b40b626a35
Vladimir Shubarin <vhundef@gmail.com>
2020-09-05 17:44:24 +0300

    initial commit

---
+++ main.cpp
@@ -0,0 +1,7 @@
+#if defined(linux) || defined(_WIN32)
+#include <GL/glut.h> /*Для Linux и Windows*/
+#else
+#include <GLUT/GLUT.h> /*Для Mac OS*/
+#endif
+
+int main(int argc, char *argv[]) { return 0; }
