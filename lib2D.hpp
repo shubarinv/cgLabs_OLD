874f6a6e12e9a70498cbc9ab9c930e83c22ece0c
Vladimir Shubarin <vhundef@gmail.com>
2020-09-21 21:43:11 +0300

    Added lib for drawing 2d primitives

---
+++ lib2D.hpp
@@ -0,0 +1,80 @@
+//
+// Created by Vladimir Shubarin on 9/11/20.
+//
+#define GL_SILENCE_DEPRECATION
+
+#ifndef CGLABS__LIB2D_HPP_
+#define CGLABS__LIB2D_HPP_
+#include <GLUT/glut.h>
+#include <OpenGL/gl.h>
+#include <vector>
+#include <cmath>
+
+template<typename T>
+struct vec3 {
+  T a, b, c;
+};
+template<typename T>
+struct vec2 {
+  T a, b;
+};
+
+/**
+ * @brief Draws a point on the screen
+ * @param _x location by x, should be between [-1;1]
+ * @param _y location by y, should be between [-1;1]
+ * @param
+ * @example drawPoint(0.5,0.5,2,{0.5,0.5,0.5});
+**/
+void drawPoint(int _x, int _y, int pointSize, vec3<float> _color) {
+  glColor3f(_color.a, _color.b, _color.c);
+  glPointSize(pointSize);
+  glEnable(GL_POINT_SMOOTH);
+  glBegin(GL_POINTS);
+  glVertex2f(0.3, 0.3);
+  glEnd();
+  glDisable(GL_POINT_SMOOTH);
+}
+void drawLine(vec2<vec2<float>> lineCoords, vec3<float> _color) {
+  glColor3f(_color.a, _color.b, _color.c);
+  glEnable(GL_LINE_SMOOTH);
+  glBegin(GL_LINES);
+  glVertex2f(lineCoords.a.a, lineCoords.a.b);
+  glVertex2f(lineCoords.b.a, lineCoords.b.b);
+  glEnd();
+  glDisable(GL_LINE_SMOOTH);
+}
+void drawTriangle(vec3<vec2<float>> triangleSides, vec3<float> _color) {
+  glColor3f(_color.a, _color.b, _color.c);
+  glEnable(GL_LINE_SMOOTH);
+  glBegin(GL_TRIANGLES);
+  glVertex2f(triangleSides.a.a, triangleSides.a.b);
+  glVertex2f(triangleSides.b.a, triangleSides.b.b);
+  glVertex2f(triangleSides.c.a, triangleSides.c.b);
+  glEnd();
+  glDisable(GL_LINE_SMOOTH);
+}
+void drawPolygon(const std::vector<vec2<float>>& polygonLines, vec3<float> _color) {
+  glColor3f(_color.a, _color.b, _color.c);
+  glEnable(GL_LINE_SMOOTH);
+  glBegin(GL_POLYGON);
+  for (auto &line:polygonLines) {
+	glVertex2f(line.a, line.b);
+  }
+  glEnd();
+  glDisable(GL_LINE_SMOOTH);
+}
+void drawCircle(float cx, float cy, float r, int num_segments, vec3<float> _color) {
+  glColor3f(_color.a, _color.b, _color.c);
+  glEnable(GL_LINE_SMOOTH);
+  glBegin(GL_LINE_LOOP);
+  for (int ii = 0; ii < num_segments; ii++) {
+	float theta = 2.0f * 3.1415926f * float(ii) / float(num_segments);//get the current angle
+	float x = r * cosf(theta);//calculate the x component
+	float y = r * sinf(theta);//calculate the y component
+	glVertex2f(x + cx, y + cy);
+  }
+  glEnd();
+  glDisable(GL_LINE_SMOOTH);
+}
+#endif // CGLABS__LIB2D_HPP_
--- main.cpp
+++ main.cpp
@@ -1,7 +1,3 @@
-#if defined(linux) || defined(_WIN32)
-#include <GL/glut.h> /*Для Linux и Windows*/
-#else
-#include <GLUT/GLUT.h> /*Для Mac OS*/
-#endif
+
 
 int main(int argc, char *argv[]) { return 0; }
