c6bf1390886ae9309ef660e7e91ba8f126b42e52
Vladimir Shubarin <vhundef@gmail.com>
2020-09-22 00:06:32 +0300

    screen.hpp was transitioned to GLFW

--- Libs/vhundef/screen.hpp
+++ Libs/vhundef/screen.hpp
@@ -5,25 +5,41 @@
 #ifndef CGLABS__SCREEN_HPP_
 #define CGLABS__SCREEN_HPP_
 #define GL_SILENCE_DEPRECATION
-#include <GLUT/glut.h>
-#include <OpenGL/gl.h>
+#include <glfw3.h>
 #include <string>
+#include <iostream>
 #include "lib2D.hpp"
+#include "../aixlog.hpp"
 
 class Screen {
-  bool initialised = false;
  public:
-  [[maybe_unused]] Screen(int argc, char **argv) {
-	// инициализация
-	glutInit(&argc, argv);
-	glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA);
-	initialised = true;
-  }
-  [[maybe_unused]] void createWindow(vec2<int> position, vec2<int> size, const std::string &windowName = "") const {
-	if (!initialised) { throw std::runtime_error("Glut was not initialised"); }
-	glutInitWindowPosition(position.a, position.b);
-	glutInitWindowSize(size.a, size.b);
-	glutCreateWindow(windowName.c_str());
+  Screen() = default;
+  static GLFWwindow *createWindow(vec2<int> position, vec2<int> size, const std::string &windowName = "") {
+	if (glfwInit()) {
+	  LOG(INFO)
+		  << "Creating new window with name: " + windowName + " and size: " + std::to_string(size.a) + "x" + std::to_string(size.b) +
+			  " px\n";
+	  GLFWwindow *window = glfwCreateWindow(size.a, size.b, windowName.c_str(), nullptr, nullptr);
+	  if (window == nullptr) {
+		LOG(ERROR) << "Cannot create window!\n";
+		throw std::runtime_error("Failed to create window!");
+	  }
+	  LOG(INFO) << "Window created successfully\n";
+	  glfwSetWindowPos(window, position.a, position.b);
+	  glfwMakeContextCurrent(window);
+	  glfwSwapInterval(1);
+	  glfwShowWindow(window);
+
+	  // Disabling ability to resize window
+	  int w, h;
+	  glfwGetWindowSize(window, &w, &h);
+	  glfwSetWindowSizeLimits(window, w, h, w, h);
+	  LOG(DEBUG) << "Disabled ability to resize window\n";
+	  return window;
+	} else {
+	  LOG(ERROR) << "GLFW was not initialised!\n";
+	  throw std::runtime_error("Failed to init GLFW");
+	}
   }
 };
 
--- main.cpp
+++ main.cpp
@@ -6,39 +6,35 @@ void handleKeyboard() {
   LOG(DEBUG) << "Checking keyboard state\n";
   if (keyStates['q'] && bIsCtrlPressed) {
 	LOG(INFO) << "Got quit command, destroying window\n";
-	glutDestroyWindow(glutGetWindow());
-	if (glutGetWindow() == 0) {
-	  LOG(DEBUG) << "Window was destroyed successfully\n";
-	  LOG(DEBUG) << "Quiting...\n";
-	  exit(0);
-	}
-	LOG(ERROR) << "Error occurred while destroying window!\n";
-	throw std::runtime_error("Failed to close window!");
+	LOG(DEBUG) << "Window was destroyed successfully\n";
+	LOG(DEBUG) << "Quiting...\n";
+	exit(0);
   }
+  LOG(ERROR) << "Error occurred while destroying window!\n";
+  throw std::runtime_error("Failed to close window!");
 }
 
 void renderScene() {
   glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
   handleKeyboard();
-  glutSwapBuffers();
 }
 
 int main(int argc, char *argv[]) {
   keyStates = new bool[256];
   AixLog::Log::init<AixLog::SinkCout>(AixLog::Severity::trace);
   LOG(INFO) << "Hello, World!\n";
-  Screen screen(argc, argv);
-  screen.createWindow({100, 100}, {200, 200}, "Test");
-  LOG(DEBUG) << "Window Created\n";
-  // регистрация обратных вызовов
-  glutDisplayFunc(renderScene);
-  glutKeyboardFunc(onKeyPressed);
-  glutKeyboardUpFunc(onKeyUp);
-  glutIdleFunc(renderScene);
+  GLFWwindow *window = Screen::createWindow({100, 100}, {200, 200}, "Test");
 
+  while (glfwWindowShouldClose(window) == GL_FALSE) {
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glColor4f(1, 0, 0, 1);
+	drawCircle(0, 0, 0.5, 50, {255, 0, 0});
 
-  // Основной цикл GLUT
-  glutMainLoop();
+	glfwSwapBuffers(window);
+	glfwPollEvents();
+  }
+  glfwDestroyWindow(window);
+  glfwTerminate();
   LOG(INFO) << "Program quit\n";
   return 0;
 }
